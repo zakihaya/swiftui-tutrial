fef7ac4e6de23e53d0c5351475aa26472a612633
hayazaki <toru.hayazaki@gmail.com>
2021-10-18 13:40:55 +0900

    Initial Commit

---
+++ swiftui-tutrial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-tutrial
+//
+//  Created by haayzaki on 2021/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-tutrial/swiftui_tutrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutrialApp.swift
+//  swiftui-tutrial
+//
+//  Created by haayzaki on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tutrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
