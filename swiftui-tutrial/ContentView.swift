18a33fe6735d3fbe27c3f432f06b742ab930e09d
hayazaki <toru.hayazaki@gmail.com>
2021-11-09 13:22:24 +0900

    DragGesture 画像をドラッグする

--- swiftui-tutrial/ContentView.swift
+++ swiftui-tutrial/ContentView.swift
@@ -18,6 +18,9 @@ struct ContentView: View {
                     NavigationLink(destination: GestureRecognizeView()) { Text("Move to GestureRecognizeView")
                     }
                     .navigationBarTitleDisplayMode(.inline)
+                    NavigationLink(destination: DrugGestureView()) { Text("Move to DrugGestureView")
+                    }
+                    .navigationBarTitleDisplayMode(.inline)
                 }
             }
             .toolbar {
@@ -33,6 +36,38 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Main Screen")
+        }
+    }
+}
+
+struct DrugGestureView: View {
+    @State var position: CGSize = CGSize(width: 200, height: 300)
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                self.position = CGSize(
+                    width: value.startLocation.x + value.translation.width,
+                    height: value.startLocation.y + value.translation.height
+                )
+            }
+            .onEnded { value in
+                self.position = CGSize(
+                    width: value.startLocation.x + value.translation.width,
+                    height: value.startLocation.y + value.translation.height
+                )
+            }
+    }
+
+    var body: some View {
+        VStack {
+            Text("x: \(self.position.width)")
+            Text("y: \(self.position.height)")
+            Image("dog1")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100.0, height: 100.0)
+                .position(x: position.width, y: position.height)
+                .gesture(drag)
         }
     }
 }
